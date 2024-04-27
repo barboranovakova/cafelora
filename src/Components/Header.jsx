f0f07a044ec109f9650f2df65793bda472d7312d
Barbora Nováková <barun.novak@gmail.com>
2024-04-27 10:14:42 +0200

    vytvoření komponent

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+export const Header = () => {
+  return (
+    <header>
+      <div className="header__content container">
+        <div className="site-logo"></div>
+
+        <div className="navigation">
+          <button className="nav-btn"></button>
+          <nav className="rollout-nav nav-closed">
+            <a href="#home">domů</a>
+            <a href="#menu">menu</a>
+            <a href="#gallery">galerie</a>
+            <a href="#contact">kontakt</a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
